06f85681e94705dab673dc9261a2caaf5327ea1b
zeina240 <zeinahanani@gmail.com>
2026-03-10 15:33:24 +0200

    share the project togather

---
+++ src/main/java/com/wasel/backend/MobilityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wasel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilityBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wasel/backend/MobilityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasel.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
