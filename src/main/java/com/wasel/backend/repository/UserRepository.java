6422306cfecc6b2c3edebd69f3b57c1ec634989a
Masa22-QN <s12217682@stu.najah.du>
2026-03-18 14:11:31 -0700

    initial commi

--- src/main/java/com/wasel/backend/repository/UserRepository.java
+++ src/main/java/com/wasel/backend/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.wasel.backend.repository;
 
-public class UserRepository {
+import com.wasel.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
