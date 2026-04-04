b96b7edcb991c2bb246ce5e854f210d28ad77727
khade <khadejahamoodah@gmail.com>
2026-04-04 20:41:43 +0300

    changes for repasing

--- src/main/java/com/wasel/backend/repository/IncidentRepository.java
+++ src/main/java/com/wasel/backend/repository/IncidentRepository.java
@@ -1,4 +1,7 @@
 package com.wasel.backend.repository;
 
-public class IncidentRepository {
+import com.wasel.backend.model.Incident;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IncidentRepository extends JpaRepository<Incident, Integer> {
 }
