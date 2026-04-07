4d1ac8b96493de1d5389b7a18137838374428b42
khade <khadejahamoodah@gmail.com>
2026-04-07 20:02:41 +0300

    rout estemation and wether emplemintation

--- src/main/java/com/wasel/backend/config/AppConfig.java
+++ src/main/java/com/wasel/backend/config/AppConfig.java
@@ -1,4 +1,22 @@
 package com.wasel.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        // ⏱️ Timeout settings
+        factory.setConnectTimeout(5000); // 5 ثواني للاتصال
+        factory.setReadTimeout(5000);    // 5 ثواني للقراءة
+
+        return new RestTemplate(factory);
+    }
 }
